b48d706b5bba9604b133a7291d488f94968860c6
Joey Teng <joey.teng.dev@gmail.com>
2016-07-19 22:17:35 +0800

    Add default timeout and U-A

--- src/default.py
+++ src/default.py
@@ -4,9 +4,11 @@
 class downloader(object):
     __slots__ = ()
     parms = None
-    headers = None
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
+    }
     allow_redirects = True
-    timeout = None
+    timeout = 3 # 3 seconds
     proxies = None
 
     def __init__(self):
