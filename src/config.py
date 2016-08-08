899c7491badf4af684e091fa625f4f2f1608ecdf
Joey Teng <joey.teng.dev@gmail.com>
2016-08-09 01:19:46 +0800

    Rewrite config.py to fix bugs and make it function.

--- src/config.py
+++ src/config.py
@@ -2,11 +2,10 @@
 #--coding:utf-8--
 
 class Config(object):
-    __slots__ = ('_attr', '_lock')
+    __slots__ = ('_lock', '__dict__')
 
     def __init__(self):
-        self._attr = dict()
-        self._lock = False
+        self.__dict__['_lock'] = False # To avoid check __getattr__() and for uniform rule
 
     def lock(self):
         self._lock = True
@@ -14,24 +13,26 @@ class Config(object):
     def release(self):
         self._lock = False
 
-    def __getattr__(self, attr):
-        if attr in self.__attr:
-            return self.__attr[attr]
-        else:
-            if self._lock:
-                return None
-
-            self.__attr[attr] = None
-            return self.__attr[attr]
-
-    def __delattr__(self, attr):
+    def __getattr__(self, name):
         try:
-            self.__attr.pop(attr)
+            return self.__dict__[name]
         except KeyError:
-            raise AttributeError("'%s' object has no attribute '%s'" %(__name__, attr))
+            raise AttributeError("'%s' object has no attribute '%s'" %(__name__, name))
 
-    def del_attr(self, attr):
-        self.__delattr__(attr)
+    def __setattr__(self, name, value):
+        if self._lock and (not self.__dict__.has_key(name)):
+            raise AttributeError("'%s' object is locked" %(__name__))
+        else:
+            self.__dict__[name] = value
+
+    def __delattr__(self, name):
+        try:
+            self.__dict__.pop(name)
+        except KeyError:
+            raise AttributeError("'%s' object has no attribute '%s'" %(__name__, name))
+
+    def del_attr(self, name):
+        self.__delattr__(name)
 
 class Downloader(Config):
     pass
