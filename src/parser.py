96c9b20b5a5a14fa1f5da5239daee1652a65554a
Joey Teng <joey.teng.dev@gmail.com>
2016-08-09 02:15:59 +0800

    Parser.py complete. config.json for Yahoo! Finance Update requirement.txt

--- src/parser.py
+++ src/parser.py
@@ -1,30 +1,64 @@
 #parser.py
 #--coding:utf-8--
 
-from bs4 import BeautifulSoup
+import feedparser
 
 import default
 import config
+import store
 
 class Parser(config.Config):
 
-    def __call__(self, data=None, template=None, config=None):
-        if data == None:
-            raise ValueError("Do Not Call This Class Without Input")
-
-        for value in self.parse(data, template=template, config=config):
-            yield value
+    def __init__(self, data=None, template=None, config=None):
+        pass
 
     def release(self):
         # Ban the usage of release lock
         raise AttributeError("'%s' object has no attribute 'release'" %(__name__))
 
-    def parse(data, template=default.parser.template, config=default.parser.config):
-        yield data
+    @classmethod
+    def parse(cls, data, template=default.parser.template, config=default.parser.config):
+        #try:
+            feedparsed = feedparser.parse(data)
+            parsed = cls.extract(feedparsed, template)
+            return parsed
+            store.save_data(parsed)
+            return True
+        #except:
+        #    return False
+
+    @classmethod
+    def extract(cls, data, template):
+        try:
+            _type = template['_type']
+            result = {}
+            if _type == 'list':
+                result = {}
+                result['_type'] = 'list'
+                _data = []
+                for element in data:
+                    _data.append(cls.extract(element, template['_data']))
+                result['_data'] = _data
+            elif _type == 'data':
+                result = data
+            elif _type == 'dict':
+                result = {}
+                for item in template.items():
+                    if item[0][0] == '_' and item[0][1:].isalnum() and item[0] != '_data': # filter control/instruction items
+                        continue
+                    result[item[0]] = cls.extract(data[item[0]], item[1])
+            elif _type == 'id':
+                result = {'_data': data, '_type': 'id'}
+            elif _type == 'time':
+                result = {'_data': data, '_type': 'time'}
+
+            return result
+
+        except KeyError:
+            return data
 
 def parse(data, template=default.parser.template, config=default.parser.config):
-    for value in Parser(data=data, template=template, config=config):
-        yield value
+    return Parser.parse(data=data, template=template, config=config)
 
 if __name__ == '__main__':
     raise EnvironmentError("Do Not Directly Run This Script")
