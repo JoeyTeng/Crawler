7bb0116202a435859e5e4b4b6b8930ddfca5ce1f
Joey Teng <joey.teng.dev@gmail.com>
2016-08-10 21:07:49 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,12 @@
 #__init__.py
 #--coding:utf-8--
 #import all modules
+import actor
 import config
 import default
 import downloader
-import manager
+import interpreter
 import parser
 import store
-import worker
+import task_loader
+import wrapper
