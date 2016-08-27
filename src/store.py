30b485b50276e9d4b60a9a000da9a3dfcc662aa5
Joey Teng <joey.teng.dev@gmail.com>
2016-08-27 16:31:24 +0800

    Add timestamp in storage

--- src/store.py
+++ src/store.py
@@ -1,9 +1,10 @@
-#store.py
-#--coding:utf-8--
+# store.py
+# --coding:utf-8--
 
 import time
 import pymongo
 
+import syntactic_sugar
 import default
 
 if __name__ == '__main__':
@@ -15,19 +16,25 @@ config = default.store
 
 MONGO_CONN = pymongo.MongoClient(config.host, config.port)
 
-def save_data(data, _id=None, _time=None, collection=None):
-    if _id == None:
+
+def save_data(data, _id=None, _time=None, collection=None, database=None):
+    if _id is None:
         data['_id'] = data['_id']
         _id = data['_id']
-    if _time == None:
+    if _time is None:
         gmt = time.gmtime()
         data['UpdateTime'] = time.asctime(gmt)
-        _time = time.mktime(gmt)
-    if collection == None:
-        collection = config.collection
 
-    MONGO_CONN[config.database][collection].update_one(
-            filter={'_id': _id},
+    collection = collection or config.collection
+    database = database or config.database
+
+    # UTC time in s since Epoch
+    _timestamp = data.get('_timestamp') or (time.time(
+    ) + (time.altzone if time.daylight else time.timezone))
+    data['_timestamp'] = _timestamp
+
+    with syntactic_sugar.suppress(pymongo.errors.DuplicateKeyError):
+        MONGO_CONN[database][collection].update_one(
+            filter={'_id': _id, '_timestamp': {'$lt': _timestamp}},
             update={'$set': data},
-            upsert=True
-    )
+            upsert=True)
