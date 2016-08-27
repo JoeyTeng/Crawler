359884c3328fbdabd172956b5598091e5ebf4694
Joey Teng <joey.teng.dev@gmail.com>
2016-08-27 22:47:11 +0800

    Improve naming & stylish

--- src/mail.py
+++ src/mail.py
@@ -12,7 +12,7 @@ import default
 class Mail(object):
 
     def __init__(self, config=None):
-        self.config = config or default.Mail.config
+        self.config = config or default.mail.config
 
     @classmethod
     def _format_addr(cls, addr_arg):
@@ -21,29 +21,29 @@ class Mail(object):
                                        addr.encode('utf-8') if isinstance(addr, unicode) else addr))
 
     def send(self, content):
-        from_addr = self.config.from_addr
+        addresser = self.config.addresser
         password = self.config.password
-        to_addr = self.config.to_addr
+        addressee = self.config.addressee
         smtp_server = self.config.smtp_server
         smtp_port = self.config.smtp_port
 
         msg = email.mime.text.MIMEText(content, 'plain', 'utf-8')
-        msg['From'] = self._format_addr(u'Financial Information Assistant <%s>' %from_addr)
-        msg['To'] = self._format_addr(u'Subscriber <%s>' %to_addr)
+        msg['From'] = self._format_addr(u'Financial Information Assistant <%s>' %addresser)
+        msg['To'] = self._format_addr(u'Subscriber <%s>' %addressee)
         msg['Subject'] = email.header.Header(u'Please read this news', 'utf-8').encode()
 
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
         server.set_debuglevel(1)
-        server.login(from_addr, password)
-        server.sendmail(from_addr, [to_addr], msg.as_string())
+        server.login(addresser, password)
+        server.sendmail(addresser, [addressee], msg.as_string())
         server.quit()
 
 def send(content, config_dict):
     _config = config.Config()
-    _config.from_addr = config_dict['from_addr']
+    _config.addresser = config_dict['addresser']
     _config.password = config_dict['password']
-    _config.to_addr = config_dict['to_addr']
+    _config.addressee = config_dict['addressee']
     _config.smtp_server = config_dict['smtp_server']
     _config.smtp_port = config_dict['smtp_port']
 
